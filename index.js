33ded44c708f33a56324b43d5f9486c5574fd001
Mihail Leontev <leontevmihail240@gmail.com>
2024-11-22 13:22:58 +0300

    generate

---
+++ index.js
@@ -0,0 +1,23 @@
+async function* generateSequence(start, end) {
+
+  for (let i = start; i <= end; i++) {
+
+    // ура, можно использовать await!
+    await new Promise(resolve => setTimeout(resolve, 0));
+
+    yield i;
+  }
+
+}
+
+(async () => {
+
+  let generator = generateSequence(1, 5);
+  for await (let value of generator) {
+    console.log(value); // 1, потом 2, потом 3, потом 4, потом 5
+  }
+
+})();
+
+
+console.log(import.meta)
