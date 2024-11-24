30a7a27b83d4c83877d471a99f7163182a3013bb
Mihail Leontev <leontevmihail240@gmail.com>
2024-11-24 11:46:58 +0300

    1

--- index.js
+++ index.js
@@ -1,22 +1,8 @@
-async function* generateSequence(start, end) {
-
-  for (let i = start; i <= end; i++) {
-
-    // ура, можно использовать await!
-    await new Promise(resolve => setTimeout(resolve, 0));
-
-    yield i;
-  }
-
-}
-
-(async () => {
-
-  let generator = generateSequence(1, 5);
-  for await (let value of generator) {
-    console.log(value); // 1, потом 2, потом 3, потом 4, потом 5
-  }
-
-})();
-
-
+const http = require("http");
+http.createServer(function(request,response){
+     
+    response.end("Hello NodeJS!");
+     
+}).listen(3000, "127.0.0.1",function(){
+    console.log("Сервер начал прослушивание запросов на порту 3000");
+});
