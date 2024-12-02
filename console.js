8e8e277b896d5453a7f91d778d0d95f520c7f352
Mihail Leontev <leontevmihail240@gmail.com>
2024-12-02 14:12:20 +0300

    Json project

--- console.js
+++
@@ -1,6 +0,0 @@
-console.log("js")
-
-
-
-
-console.log("yuil")
--- index.js
+++ index.js
@@ -1,8 +1,60 @@
 const http = require("http");
-http.createServer(function(request,response){
-     
-    response.end("Hello NodeJS!")
-     
-}).listen(3000, "127.0.0.1",function(){
-    console.log("Сервер начал прослушивание запросов на порту 3000");
+const fs = require("fs");
+const path = require("path")
+  
+
+/*
+let data = {"null": "123"}
+
+fs.writeFile('as.json', JSON.stringify(data), function(error){
+    if(error){  // если ошибка
+        return console.log(error);
+    }
+    console.log("Файл успешно записан");
 });
+
+
+
+fs.readFile(path.resolve(__dirname, 'as.json'), function(err, data){
+    if (err){
+        return console.log(err)
+    }
+    console.log(data.toString())
+})
+*/
+
+http.createServer(async (request, response) => {
+        
+    if(request.url == "/user"){
+           
+          let body = "";   // буфер для получаемых данных
+         // получаем данные из запроса в буфер
+          for await (const chunk of request) {
+            body += chunk;
+          }
+        // для параметра name
+        let userName = "";
+        // для параметра age
+        let userAge = 0;
+        // разбиваем запрос на параметры по символу &
+        const params = body.split("&");
+        // проходим по всем параметрам и определяем их значения
+        let obj = {}
+        for(param of params){
+            // разбиваем каждый параметр на имя и значение
+            const [paramName, paramValue] = param.split("=");
+            if(paramName === "username") userName = paramValue; obj["username"]=userName;
+            if(paramName === "userage") userAge = paramValue; obj["userage"]=userAge;
+        }
+        fs.writeFile(path.resolve(__dirname, 'as.json'), JSON.stringify(obj), function(err){
+            if(err){
+                return console.log(err)
+            }
+        })
+        response.end(`Your name: ${userName}  Your Age: ${userAge}`);
+        
+    }
+    else{
+        fs.readFile("form.html", (_, data) => response.end(data));
+    }
+}).listen(3000, ()=>console.log("Сервер запущен по адресу http://localhost:3000"));
