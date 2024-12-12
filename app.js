00b7e0c691cec9d749dd016640992e1a90eba06c
Mihail Leontev <leontevmihail240@gmail.com>
2024-12-12 22:58:00 +0300

    first commit

--- app.js
+++ app.js
@@ -2,75 +2,99 @@ const express = require('express');
 const { v4: uuidv4 } = require('uuid');
 
 const app = express()
-
 app.use(express.json())
 
-const bd = [{ "id": 1, "name": "Alice", "email": "alice@example.com", "age": 25 }];
+bd = [{
+    "id": 1,
+    "title": "Harry Potter",
+    "author": "J.K. Rowling",
+    "genre": "Fantasy",
+    "year": 1997
+  }]
+
+function small(name_par, id){
+    for(let i = 0; i < bd.length; i++){
+        if(bd[i][`${name_par}`] == id){
+            return bd[i]
+        }
+    }
+}
+
+function searchs(name, author, id){
+    if (id != null){
+        return small("id", id)
+    }
+    else if (name != null){
+        return small("name", name)
+    }
+    else if(author != null){
+        return small("author", author)
+    }
+    return false
+}
+
+app.get('/books', function(request, response){
+    const parm = request.query;
+
+    if(parm.author != undefined || parm.title != undefined){
+        if (parm.title != undefined){
+            response.json(searchs(parm.title, null, null))
+        }
+        else if(parm.author != undefined){
+            response.json(searchs(null, parm.author, null))
+        }
+        response.send(parm)
+    }
+
+    else{
+        response.json(bd)
+    }
+})
 
 
-app.get('/user', function(_, response){
-    response.json(bd)
-});
+app.get('/books/:id', function(request, response){
+    const id = request.params['id']
+    response.json(searchs(null, null, id))
+})
 
-app.get('/user/:id', function(request, response){
+
+app.post('/books', function(request, response){
+    if (!request.body){return response.sendStatus(400)}
+
+    const id = uuidv4();
+    const title = request.body.title;
+    const author = request.body.author;
+    const genre = request.body.genre;
+    const year = request.body.year;
+
+    bd.push({'id': id, 'title': title, 'author': author, 'genre': genre, 'year': year})
+
+    response.json({'id': id, 'title': title, 'author': author, 'genre': genre, 'year': year})
+})
+
+
+app.put('/books/:id', function(request, response){
+    if (!request.body){return response.sendStatus(400)}
+
     const id = request.params['id'];
-    let flag = 0
-    for (let i = 0; i < bd.length; i++){
-        if (bd[i]["id"] == id){
-            response.json(bd[id-1]);
-            flag++
+    const book = searchs(null, null, id);
+
+    book.title = request.body.title;
+    book.author = request.body.author;
+    book.genre = request.body.genre;
+    book.year = request.body.year;
+
+    for(let i = 0; i < bd.length; i++){
+        if(bd[i][`${id}`] == book.id){
+            bd[i] = book
         }
     }
-    if(!flag){
-        response.json({ error: "User not found" }).sendStatus(404)
-    }
+
+    response.json(book)
 })
 
 
-app.post('/user', function(request, response){
-    if(!request.body) return response.sendStatus(400);
-    const user = request.body
-    
-    user.id = uuidv4();
-
-    bd.push(user)
-    response.json(user)
-})
-
-
-
-
-app.put('/user/:id', function(request, response){
-    if(!request.body) return response.sendStatus(400);
-
-
-    const id = Number(request.params['id']);
-    let ind
-
-    for (let i = 0; i < bd.length; i++){
-        if (bd[i]["id"] == id){
-            ind = i
-        }
-    }
-    if(ind == undefined){
-        return response.json({ error: "User not found" }).sendStatus(404)
-    }
-
-
-    const name = request.body.name;
-    const email = request.body.email;
-    const age = request.body.age;
-
-    bd[ind] = {"id": id, "name": name, "email": email, "age": age}
-
-    response.json({"id": id, "name": name, "email": email, "age": age})
-})
-
-
-
-
-
-app.delete('/user/:id', function(request, response){
+app.delete('/books/:id', function(request, response){
     const id = request.params['id'];
 
     let flag = 0
@@ -89,4 +113,4 @@ app.delete('/user/:id', function(request, response){
 
 
 
-app.listen(4000, () => { console.log('Сервер запущен') });
+app.listen(4000, () => { console.log('Сервер работает') })
